e49c2f06c9bf02f661dd225b9041f51f1af61551
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-20 23:02:03 +0900

    add models

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,11 @@
 class Experience < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 
+  # validations
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :duration, presence: true
 end
