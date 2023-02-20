411592595058625087e0b30da89d1f8182d8d031
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-20 23:04:46 +0900

    create bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_125900) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_135644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.integer "status"
+    t.bigint "user_id", null: false
+    t.bigint "experience_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["experience_id"], name: "index_bookings_on_experience_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "name"
@@ -40,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_125900) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "experiences"
+  add_foreign_key "bookings", "users"
   add_foreign_key "experiences", "users"
 end
