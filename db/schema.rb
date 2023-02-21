30cf937f9807719a916e2b36e41554d9b4d5b285
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-21 23:20:10 +0900

    add booking status 0 as default

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_135644) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_141359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.datetime "start_date"
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "user_id", null: false
     t.bigint "experience_id", null: false
     t.datetime "created_at", null: false
