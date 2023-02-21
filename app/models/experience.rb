43e1379d2edb53bfc6eaa076a540059e86b1e2a5
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-21 22:15:26 +0900

    add dependences on experience

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,7 @@
 class Experience < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+  has_many :user, through: :bookings
 
   # validations
   validates :name, presence: true
