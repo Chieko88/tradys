49c536dd4d1691ab9508fb40531af9b1b2565a15
Chieko Sato <chiekosato.02@gmail.com>
2023-02-23 18:10:23 +0900

    fixed booking form style

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,5 @@ class Booking < ApplicationRecord
 
   # validations
   validates :start_date, presence: true
+  validates :number_of_people, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
