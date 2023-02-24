8be5a0c87979cd3c8189d01c2ccccdf4d491a05a
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-24 15:39:05 +0900

    fix name of experience on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,22 +45,22 @@ guest_user.photo.attach(io: file, filename: 'user.png', content_type: 'image/png
 
 
 # Sample EXPERIENCES
-experience = Experience.new(name: "Challenge Sumo Wrestlers and Enjoy Lunch", description: "Eat, train, and fight like a real Japanese sumo wrestler during this sumo demonstration and authentic Chanko Nabe (hotpot) meal. Discover the history, training, and rules behind the typical sumo match as you watch real clashes. You can even try it yourself during a skirmish with a retired wrestler. Once you’ve worked up an appetite, dig into a tasty Chanko Nabe, a filling meal of hotpot, rice, miso, salad, French fries and more, used by these hulking fighters to gain weight. Admission to the demonstration and meals are both covered.", price: 14500, duration: 180 , address: "Sumida City, Japan", user: host_user )
+experience = Experience.new(name: "Challenge Sumo Wrestlers", description: "Eat, train, and fight like a real Japanese sumo wrestler during this sumo demonstration and authentic Chanko Nabe (hotpot) meal. Discover the history, training, and rules behind the typical sumo match as you watch real clashes. You can even try it yourself during a skirmish with a retired wrestler. Once you’ve worked up an appetite, dig into a tasty Chanko Nabe, a filling meal of hotpot, rice, miso, salad, French fries and more, used by these hulking fighters to gain weight. Admission to the demonstration and meals are both covered.", price: 14500, duration: 180 , address: "Sumida City, Japan", user: host_user )
 file = File.open("app/assets/images/sumopic.jpg")
 experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/png")
 experience.save
 
-experience = Experience.create!(name: "Adventurous Private Japanese Food Tour", description: "Taste some of Japan’s most notorious delicacies on a private food tour of Tokyo’s Ginza district. Find the food experiences you want and order with the help of your English speaking guide. Open your eyes to local foods you didn’t know existed, or wouldn’t have tried on your own. If you want to try blowfish, horse sashimi, or whale, this is your tour; either way, your guide can adjust the menu and restaurant visits to meet your needs.", price: 17500, duration: 120 , address: "Minato City, Japan", user: User.all.sample )
+experience = Experience.create!(name: "Adventurous Japanese Food Tour", description: "Taste some of Japan’s most notorious delicacies on a private food tour of Tokyo’s Ginza district. Find the food experiences you want and order with the help of your English speaking guide. Open your eyes to local foods you didn’t know existed, or wouldn’t have tried on your own. If you want to try blowfish, horse sashimi, or whale, this is your tour; either way, your guide can adjust the menu and restaurant visits to meet your needs.", price: 17500, duration: 120 , address: "Minato City, Japan", user: User.all.sample )
 file = File.open("app/assets/images/foodtourpic.jpg")
 experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/jpg")
 experience.save
 
-experience = Experience.create!(name: "Cooking Class and Lunch at a Tuscan Farmhouse with Local Market Tour from Florence", description: "Discover the secrets of Tuscan cooking during this full-day experience, including a market visit and cooking class, topped off with a 4-course lunch. Shop for fresh ingredients at a Florentine market before venturing to a Tuscan farmhouse in the countryside. Take your cue from an expert chef as you create Italian dishes from scratch including bruschetta, pasta, roast pork, and tiramisù. Then sit down to enjoy the fruits of your labor, complemented by Tuscan wine.", price: 21000, duration: 180 , address: "Firenze, Italy", user: host_user )
+experience = Experience.create!(name: "Cooking Class at a Tuscan Farmhouse", description: "Discover the secrets of Tuscan cooking during this full-day experience, including a market visit and cooking class, topped off with a 4-course lunch. Shop for fresh ingredients at a Florentine market before venturing to a Tuscan farmhouse in the countryside. Take your cue from an expert chef as you create Italian dishes from scratch including bruschetta, pasta, roast pork, and tiramisù. Then sit down to enjoy the fruits of your labor, complemented by Tuscan wine.", price: 21000, duration: 180 , address: "Firenze, Italy", user: host_user )
 file = File.open("app/assets/images/tuscanpic.jpg")
 experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/jpg")
 experience.save
 
-experience = Experience.create!(name: "Traditional Balinese Cooking Class with Tanah Lot Temple Tour", description: "Immerse yourself in Balinese culture and discover the secrets of traditional Indonesian cuisine on this full-day small-group tour. Cycle through the countryside, visit Tanah Lot Temple, and pick fresh herbs and vegetables, then create delicious local specialties such as coconut satay or banana leaf chicken during an expert-led cooking class.", price: 10500, duration: 180 , address: "Kuta, Indonesia", user: User.all.sample )
+experience = Experience.create!(name: "Traditional Balinese Cooking Class", description: "Immerse yourself in Balinese culture and discover the secrets of traditional Indonesian cuisine on this full-day small-group tour. Cycle through the countryside, visit Tanah Lot Temple, and pick fresh herbs and vegetables, then create delicious local specialties such as coconut satay or banana leaf chicken during an expert-led cooking class.", price: 10500, duration: 180 , address: "Kuta, Indonesia", user: User.all.sample )
 file = File.open("app/assets/images/balicookingpic.jpg")
 experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/jpg")
 experience.save
@@ -75,7 +75,7 @@ file = File.open("app/assets/images/flamencopic.jpg")
 experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/jpg")
 experience.save
 
-experience = Experience.create!(name: "Jellied Eel Cooking Class at London's Oldest Fast Food Restaurant", description: "Learn how to cook jellied eel, a traditional British dish at one of London's oldest fast food restaurants, F Cooke!", price: 6000, duration: 90 , address: "London, England", user: User.all.sample )
+experience = Experience.create!(name: "Jellied Eel Cooking Class", description: "Learn how to cook jellied eel, a traditional British dish at one of London's oldest fast food restaurants, F Cooke!", price: 6000, duration: 90 , address: "London, England", user: User.all.sample )
 file = File.open("app/assets/images/eelspic.jpg")
 experience.photo.attach(io: file, filename: "experience.jpg", content_type: "image/jpg")
 experience.save
